71042357e5dcfa8c4cdbddd36bda187464c5e1db
Mienz <37392161+aminmaruf221@users.noreply.github.com>
2021-07-03 03:18:49 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "db_ajc";
+    $username = "b4859a310bf81d";
+    $password = "59c8f505";
+    $database = "heroku_3ea3c8ac277ce05";
     $koneksi = mysqli_connect($host, $username, $password, $database);
 
     if(!$koneksi){
